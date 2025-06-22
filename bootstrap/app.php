9c865bd2bc4d996a20fe97e46210080eddc6958b
iyantama9 <pratamawhilyan399@gmail.com>
2025-06-22 10:16:11 +0700

    app di bootstrap nya jangan lupa di update mams dafa

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
 
-        $middleware->prepend(\App\Http\Middleware\ForceHttps::class);
         $middleware->validateCsrfTokens(except: [
             'midtrans/callback' 
         ]);
